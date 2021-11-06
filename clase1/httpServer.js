db16930aae869e16932c28863d8eae9465be999a
= <=>
2021-11-05 22:25:04 -0500

    ClaseNodeJS

---
+++ clase1/httpServer.js
@@ -0,0 +1,9 @@
+const { listenerCount } = require("process");
+
+const http = require("http")
+
+http.createServer(function(req,res){
+    console.log(req)
+res.write("Hola Mundo!!!")
+res.end();
+}).listen(8080)
